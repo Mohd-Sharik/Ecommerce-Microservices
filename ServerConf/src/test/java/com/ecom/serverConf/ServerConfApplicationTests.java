95ecafb8ced6cdf15d5b7f9dd4a85f41f0262ee7
Sharik Khan <sharikkhan9012@gmail.com>
2023-09-09 09:17:44 +0530

    successfuly ragister microservices Eureka client with Eureka Server Microservices :

---
+++ ServerConf/src/test/java/com/ecom/serverConf/ServerConfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.serverConf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
